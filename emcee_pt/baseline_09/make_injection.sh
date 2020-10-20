b635656a572ccfaaffda51ef1c0825c214e73db5
Stephanie Brown <stephanie.brown@condor1.atlas.local>
2020-10-20 14:11:13 +0000

    upload config files for several directories

---
+++ emcee_pt/baseline_09/make_injection.sh
@@ -0,0 +1,1 @@
+pycbc_create_injections --config-files injection.ini --ninjections 1 --output-file injection.hdf --verbose
---
+++ emcee_pt/baseline_09/run_workflow_detuned.sh
@@ -0,0 +1,19 @@
+set -e
+WORKFLOW_NAME=baseline_emceept_relative_09
+# Set the HTML_DIR to point to your public html page. This is where the results
+# page will be written.
+HTML_DIR='/work/stephanie.brown/WWW/cosmic_explorer/'
+if [ "${HTML_DIR}" == '' ]; then
+    echo "Please set an HTML_DIR"
+    exit 1
+fi
+SEED=983124
+# Set the number of injections to create. For a full PP test, we suggest using
+# 100.
+NINJ=1
+pycbc_make_inference_inj_workflow \
+    --seed ${SEED} \
+    --config-files workflow_config.ini baseline_injections_config.ini\
+    --workflow-name ${WORKFLOW_NAME} \
+    --injection-file  injection.hdf \
+    --config-overrides results_page:output-path:${HTML_DIR}/${WORKFLOW_NAME}
